df8b3a21c6d30db9c08d91e8bea8293af6196a98
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2025-10-25 10:12:31 +0300

    add express

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log("It Works");
+import express from 'express'
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('It Works');
+});
+
+app.listen(5000, () => console.log('Server is listening on http://localhost:5000....'));
