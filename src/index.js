18b0549d18f979bc42026d23d5ec41ef63e819d9
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2025-10-25 10:28:40 +0300

    add views folder

--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 import express from 'express'
+import handlebars from 'express-handlebars';
 
 const app = express();
 
+app.engine('hbs', handlebars.engine({
+    extname: 'hbs',
+}));
+
+app.set('view engine', 'hbs');
+
+app.set('views', './src/views');
+
 app.get('/', (req, res) => {
-    res.send('It Works');
+    res.render('home', { layout: false });
 });
 
 app.listen(5000, () => console.log('Server is listening on http://localhost:5000....'));
