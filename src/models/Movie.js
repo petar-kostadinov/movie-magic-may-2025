cc8e1f9c0ef32aea03ede576d67ff9c1e6cb024a
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2025-11-04 20:51:37 +0200

    create movie shema

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,26 +1,43 @@
-import fs from "node:fs/promises"
-import { v4 as uuid } from 'uuid'
+import { Schema, model } from "mongoose";
 
-const moviesJson = await fs.readFile('./src/database.json');
-export const movies = JSON.parse(moviesJson);
 
-export default class Movie {
-    constructor(data) {
-        this.data = data;
-    }
+const movieShema = new Schema({
+    title: String,
+    category: String,
+    genre: String,
+    director: String,
+    year: Number,
+    imageUrl: String,
+    rating: Number,
+    description: String,
+});
 
-        async save() {
+const Movie = model('Movie', movieShema);
+
+export default Movie;
+
+//import fs from "node:fs/promises"
+//import { v4 as uuid } from 'uuid'
+
+//const moviesJson = await fs.readFile('./src/database.json');
+//export const movies = JSON.parse(moviesJson);
+
+//export default class Movie {
+   // constructor(data) {
+    //    this.data = data;
+   // }
+
+   //     async save() {
         
-        this.data.id = uuid();
+   //     this.data.id = uuid();
 
-        this.data.rating = Number(this.data.rating);
+   //     this.data.rating = Number(this.data.rating);
 
-        movies.push(this.data);
+  //      movies.push(this.data);
 
-        await fs.writeFile('./src/database.json', JSON.stringify(movies, null, 4));
+  //      await fs.writeFile('./src/database.json', JSON.stringify(movies, null, 4));
 
-        return this.data;
-    }
-}
+  //      return this.data;
+  //  }
+//}
 
-//Movie.find()
