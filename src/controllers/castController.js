44792b259d6329aadae579159d318c77dd89f200
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2025-11-05 21:50:56 +0200

    save cast to DB

--- src/controllers/castController.js
+++ src/controllers/castController.js
@@ -1,9 +1,18 @@
 import express from 'express'
+import castService from '../services/castService.js';
 
 const castController = express.Router();
 
 castController.get('/create', (req, res) => {
-res.render('cast/create');
+    res.render('cast/create');
 });
+
+castController.post('/create', async (req, res) => {
+    const castData = req.body;
+
+    await castService.create(castData);
+
+    res.redirect('/');
+})
 
 export default castController;
