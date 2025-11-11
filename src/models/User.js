6768a6c790a53f487e0120156b656c52babd6bb2
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2025-11-11 20:58:36 +0200

    hash pass on save

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 import { Schema, model } from "mongoose";
+import bcrypt from 'bcrypt';
 
 const userSchema = new Schema({
     email: {
@@ -10,6 +11,11 @@ const userSchema = new Schema({
         required: true,
     }
 });
+
+userSchema.pre('save', async function () {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+})
 
 const User = model('User', userSchema);
 
