478d840bcdb117531abf872a670f0069073e2a75
petar-kostadinov <153288029+petar-kostadinov@users.noreply.github.com>
2025-11-11 21:12:29 +0200

    add login page

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,7 +4,7 @@ import userService from "../services/userService.js";
 const userController = Router();
 
 userController.get('/register', (req, res) => {
-res.render('user/register');
+    res.render('user/register');
 });
 
 userController.post('/register', async (req, res) => {
@@ -13,8 +13,12 @@ userController.post('/register', async (req, res) => {
     await userService.register(userData);
 
     res.redirect('/users/login');
-    
-    
+
+
+});
+
+userController.get('/login', (req, res) => {
+    res.render('user/login');
 });
 
 export default userController;
